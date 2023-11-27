1eb129a3ff366a01c31ebab0513eb56900d2a01c
lordburaa <comrdebiruk97@gmail.com>
2023-11-27 04:34:50 -0800

    8-concatC

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkabe power with very clear syntax"
-test = str[38:65] + str[-24:-18] + str1[0:6]
-print(test)
+str = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkabe power with very clear syntax"
+str = str[39:65] + str[-24:-17] + str[0:7]
+print(str)
