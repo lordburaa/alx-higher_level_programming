95af7cc793ce775bc56213e25a18449a1cbf76e4
lordburaa <comrdebiruk97@gmail.com>
2023-11-28 04:49:52 -0800

    all5

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(0, 9):
     j = i
     for j in range(j, 9):
-        print("{}{}".format(i,j + 1), end="")
+        print("{}{}".format(i, j + 1), end="")
         if (--i != 8 and --j != 9):
             print(", ", end="")
-print()     
+print()
