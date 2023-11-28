365b6420b0c8f134e575e5b8f901ffaf26761abe
lordburaa <comrdebiruk97@gmail.com>
2023-11-27 16:09:44 -0800

    linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * check_cycle - check if it is cylce
+ * @list: linked list
+ * Return: 0 if no scyle else 1
+ */
+int check_cycle(listint_t *list)
+{
+	if (list->next == NULL)
+		return (0);
+	else
+		return (1);
+}
