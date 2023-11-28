74d99bd6090caae2a2c5350f150f1e6b1c3dc01c
lordburaa <comrdebiruk97@gmail.com>
2023-11-28 03:32:10 -0800

    all

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import random
 number =  random.randint(-10, 10)
-print(f"{number} is negative" if (number < 0)  else f"{number} is zero"  if (number == 0) else f"{number} is positive")
+print(f"{number} is negative" if (number < 0) elif f"{number} is zero"  if (number == 0) else f"{number} is positive")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(97, 122):
+for i in range(97, 123):
     if (i == 101 or i == 113):
-        continue;
+        continue
     else:
-        print("{:c}".format(i),end ="")
+        print("{:c}".format(i), end="")
