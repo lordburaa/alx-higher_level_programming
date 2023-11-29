65380bd3ade440cf7ee18c656e422bc8c55e526e
lordburaa <comrdebiruk97@gmail.com>
2023-11-28 17:08:42 -0800

    0-is ngegtive

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 import random
 number =  random.randint(-10, 10)
-print(f"{number} is negative" if (number < 0) elif f"{number} is zero"  if (number == 0) else f"{number} is positive")
+
+print(f"{number}", end=' ')
+if (number < 0):
+    print("is negative")
+elif number == 0:
+    print("is zero")
+else:
+    print("is positive")
