dc764d8ae7cd3862716013d739e7edee98a94f6d
lordburaa <comrdebiruk97@gmail.com>
2023-11-30 04:06:00 -0800

    all argumet

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import sys
 u = len(sys.argv)
-print(f"{u - 1} argument:")
+if (u == 2):
+    print(f"{u - 1} argument")
+else:
+    print(f"{u - 1} arguments:")
 for i in range(1, u):
     print("{}: {}".format(i, sys.argv[i]))
