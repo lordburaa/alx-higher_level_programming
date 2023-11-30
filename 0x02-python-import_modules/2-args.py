f104a4d9fd31f939e6ed308712a9359f16e52aa1
lordburaa <comrdebiruk97@gmail.com>
2023-11-30 04:00:45 -0800

    all

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
 u = len(sys.argv)
+print(f"{u - 1} argument:")
 for i in range(1, u):
-    print("{} {}".format(i, sys.argv[i]))
+    print("{}: {}".format(i, sys.argv[i]))
