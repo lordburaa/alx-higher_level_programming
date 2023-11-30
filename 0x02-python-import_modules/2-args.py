aa03abe140d56781d6160b4bc0ce8ea9ac0e6c07
lordburaa <comrdebiruk97@gmail.com>
2023-11-30 04:09:07 -0800

    all

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,9 @@
 import sys
 u = len(sys.argv)
 if (u == 2):
-    print(f"{u - 1} argument")
+    print(f"{u - 1} argument:")
+elif (u == 1):
+    print(f"{u - 1} argument.")
 else:
     print(f"{u - 1} arguments:")
 for i in range(1, u):
