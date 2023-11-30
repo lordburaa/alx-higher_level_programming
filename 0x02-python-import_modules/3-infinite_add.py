baa47b994a1019f8a8c2ac0a95c18060f6dd29f0
lordburaa <comrdebiruk97@gmail.com>
2023-11-30 04:56:54 -0800

    all

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import sys
-sum = 0
+temp = int(0)
+summ = bin(0)
 u = len(sys.argv)
 for i in range(1, u):
-    sum += int(sys.argv[i])
-print(f"{sum}")
+    temp = temp + int(sys.argv[i])
+    summ = bin(temp) or summ
+
+print("{}".format((int(summ, 2))))
