5868c6ff99dd220ab7838691100fb258ba4107d5
lordburaa <comrdebiruk97@gmail.com>
2023-11-30 04:25:22 -0800

    all

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-if __name__ = "__main__"
+if __name__ == "__main__":
     import sys
 u = len(sys.argv)
 if (u == 1):
     print(f"{u - 1} arguments.")
 else:
     if u == 2:
-    print(f"{u - 1} argument.")
+        print(f"{u - 1} argument.")
     else:
         print(f"{u - 1} arguments:")
     for i in range(1, u):
