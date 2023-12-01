ec177b3b631118f6f0b6add2ff3ed00d90c0c512
lordburaa <comrdebiruk97@gmail.com>
2023-11-30 16:05:24 -0800

    all

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     import calculator_1
-k = int(-1)
+k = int(0)
 u = len(sys.argv)
 if (u < 4):
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -10,13 +10,13 @@ if (u < 4):
 operator = {'+', '-', '*', '/'}
 for i in operator:
     if (i == sys.argv[2]):
-        k = 1 + k
+        k = 2 + k
         break
     k += 1
-if (k == -1):
+if (k == 4):
     print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
-k = k - 1
+k = k - 2
 fun = [calculator_1.add, calculator_1.sub, calculator_1.mul, calculator_1.div]
 print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]),
                              int(fun[k](int(sys.argv[1]), int(sys.argv[3])))))
