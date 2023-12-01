fcae476cc6eb8f8e97b1743abe71f7cf0647a591
lordburaa <comrdebiruk97@gmail.com>
2023-11-30 16:22:25 -0800

    all

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,8 +3,8 @@ if __name__ == "__main__":
     import sys
     import calculator_1
 k = int(0)
-u = len(sys.argv)
-if (u < 4):
+u = len(sys.argv) - 1
+if (u != 3):
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
 operator = {'+', '-', '*', '/'}
@@ -18,5 +18,5 @@ if (k == 4):
     exit(1)
 k = k - 2
 fun = [calculator_1.add, calculator_1.sub, calculator_1.mul, calculator_1.div]
-print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]),
+print("{} {} {} = {}".format(int(sys.argv[1]), chr(sys.argv[2]), int(sys.argv[3]),
                              int(fun[k](int(sys.argv[1]), int(sys.argv[3])))))
