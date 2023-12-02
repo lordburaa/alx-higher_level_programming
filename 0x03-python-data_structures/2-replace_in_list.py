7fa2286186f96f7a24d8c18f67ce519aaa2b1522
lordburaa <comrdebiruk97@gmail.com>
2023-12-01 22:08:54 -0800

    all

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     leng = len(my_list)
     if (idx < 0):
@@ -7,5 +8,3 @@ def replace_in_list(my_list, idx, element):
     else:
         my_list[idx] = element
         return my_list
-
-
