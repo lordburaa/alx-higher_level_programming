7ee7e17856cdfce5a21f467f666a87bd91d0c2bb
lordburaa <comrdebiruk97@gmail.com>
2023-12-02 00:41:52 -0800

    all

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     leng = len(sentence)
-    if (leng == 0):
+    if (sentence == ""):
         sentence[0] = None
     return (leng, sentence[0])
