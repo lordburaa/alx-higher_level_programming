5a800e4ec4ee403cf5554a5b304a0c2340167caf
lordburaa <comrdebiruk97@gmail.com>
2023-12-02 03:22:30 -0800

    all

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        return
     leng = len(my_list) - 1
     new_list = my_list
     for i in range(leng, -1, -1):
