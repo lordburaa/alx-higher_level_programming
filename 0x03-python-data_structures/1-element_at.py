eaf3106dca3b5fa33a0c69b4f03eaf5b72b61722
lordburaa <comrdebiruk97@gmail.com>
2023-12-01 21:55:06 -0800

    all

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 def element_at(my_list, idx):
     num = 0
     leng = len(my_list)
-    if (idx > leng or idx < 0):
-        return
+    if (idx >= leng or idx < 0):
+        return None
 
     new_list = my_list.pop(idx)
     return new_list
