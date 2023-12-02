a34b92e3eb44690b453ff4d297e641529f0d9d8b
lordburaa <comrdebiruk97@gmail.com>
2023-12-02 06:25:32 -0800

    all

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if not my_list:
-        return
+        return None
     if (idx < 0):
         return (my_list)
     if (idx > len(my_list)):
         return (my_list)
     for i in range(len(my_list)):
         if i == idx:
-            del my_list[idx] 
+            del my_list[idx]
     return my_list
