50853eb4cfb9807a3cbabe104cdd207ed47aa93b
lordburaa <comrdebiruk97@gmail.com>
2023-12-02 01:06:05 -0800

    all

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_t = 0
+    for i in range(len(my_list)):
+        if (max_t > my_list[i]):
+            continue
+        else:
+            max_t = my_list[i]
+    return max_t
