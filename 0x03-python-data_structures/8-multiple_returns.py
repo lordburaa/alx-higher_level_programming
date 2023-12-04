164456e6586b7a8de4781b7e6768253daad52569
lordburaa <comrdebiruk97@gmail.com>
2023-12-04 05:51:03 -0800

    return modified

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence):
     leng = len(sentence)
     if leng == 0:
         sentence = None
-        return None
+        return (leng, None)
     return (leng, sentence[0],)
