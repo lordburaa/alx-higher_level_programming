1d3a50d4c9d8e916d1a2b482e454eed2ed7a4f03
lordburaa <comrdebiruk97@gmail.com>
2023-12-04 05:43:24 -0800

    returnr tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,5 @@
 def multiple_returns(sentence):
     leng = len(sentence)
     if leng == 0:
-        return None;
-    return (leng, sentence[0])
+        return None
+    return (leng, sentence[0],)
