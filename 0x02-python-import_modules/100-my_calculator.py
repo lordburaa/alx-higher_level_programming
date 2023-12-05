0fdc2e461fe4c09d400ab4d89a3ecdf894e1bcf1
lordburaa <comrdebiruk97@gmail.com>
2023-12-04 16:22:49 -0800

    all

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -14,11 +14,11 @@ if __name__ == "__main__":
         if operator == "+":
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
         elif operator == "-":
-            print("{:d} - {:d} = {:d}".format(a, b, add(a, b)))
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
         elif operator == "*":
-            print("{:d} * {:d} = {:d}".format(a, b, add(a, b)))
+            print("{:d} * {:d} = {:d}".format(a, b, mult(a, b)))
         elif operator == "/":
-            print("{:d} / {:d} = {:d}".format(a, b, add(a, b)))
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
