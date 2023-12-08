0955077ea9cd4bab8e081734fb2f1b792d1250f6
lordburaa <comrdebiruk97@gmail.com>
2023-12-08 03:26:07 -0800

    all

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,3 @@
 #!/bin/python3
 def square_matrix_simple(matrix=[]):
-    if len(matrix) == 0 or matrix is None:
-        return (None)
-    new_matrix = []
-    for each in matrix:
-        new_matrix.append([x ** 2 for x in each])
-    return (new_matrix)
+    return [[x*x for x in line] for line in matrix]
