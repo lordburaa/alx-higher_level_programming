f359603b96b90962fe81db47449abb91e1c96c2b
lordburaa <comrdebiruk97@gmail.com>
2023-12-17 00:36:28 -0800

    testing

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+
 def square_matrix_simple(matrix=[]):
-    return [[x*x for x in line] for line in matrix]
+    new = [[row[i] * row[i] for row in matrix] for i in range(3)]
+
+    return new
+#   return [[x*x for x in line] for line in matrix]
