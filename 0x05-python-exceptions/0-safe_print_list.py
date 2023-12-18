1fdf40ddbd902fbd8a596d39b283ba7ce3578459
lordburaa <comrdebiruk97@gmail.com>
2023-12-18 05:43:33 -0800

    all

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x = 10):
+def safe_print_list(my_list=[], x=0):
     cmd = 0
     for index in range(x):
         try:
             print(my_list[index], end='')
-            emd += 1
+            cmd += 1
         except Exception as error:
             break
     print('')
