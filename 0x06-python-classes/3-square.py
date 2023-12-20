04101486c0a2545e1569167057329cb5daf27f5b
lordburaa <comrdebiruk97@gmail.com>
2023-12-20 03:52:42 -0800

    3-task

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""empty square"""
+
+
+class Square:
+    """Represent square"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+    def area(self):
+        return (self.__size) ** 2
