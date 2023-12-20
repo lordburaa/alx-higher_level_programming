f659ab4731766c91ba8b37b0af163eef1a62a44d
lordburaa <comrdebiruk97@gmail.com>
2023-12-20 11:39:31 -0800

    all

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -5,7 +5,7 @@ class Square:
     """reperesents a square """
     def __init__(self, size=0):
         """itnitalize the square"""
-        self.size = size
+        self.__size = size
     def area(self):
         """calculate the square"""
         return (self.__size) ** 2
@@ -25,19 +25,20 @@ class Square:
                 self.__size = value
     def __lt__(self, other):
         """compare > than sign"""
-        return self.size < other.size
+        return self.area() < other.area()
 
     def __le__(self, other):
         """ comare  less than sign"""
-        return self.size <= other,size
+        return self.area() <= other.area()
     def __eq__(self, other):
         """compare if square is equal to another by arae"""
-        return self.size == other.size
-    def __ne__(self, othere):
+        return self.area() == other.area()
+    def __ne__(self, other):
         """compare if square is not equal to anoehr by area"""
-        return sekf.size != other.size
+        return self.area() != other.area()
     def __ge__(self, other):
         """ compare is squae is greater tha or equal to another by are """
-        return sekf.size >= other.size
+        return self.area() >= other.area()
     def __gt__(self, other):
-        return self.size > other.size
+        """compare is square i greaer than anoher by area"""
+        return self.area() > other.area()
