bf184c65afc7ffdc3ca0894baaa8655fce478778
lordburaa <comrdebiruk97@gmail.com>
2023-12-20 03:30:28 -0800

    all

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,6 +9,6 @@ class Square:
             raise TypeError("size must be an integer")
         else:
             if size < 0:
-                raise ValueError("size must be >=0")
+                raise ValueError("size must be >= 0")
             else:
                 self.__size = size
