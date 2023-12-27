698baa742bc75ee71e7f2e304dde0c638fc2db8d
lordburaa <comrdebiruk97@gmail.com>
2023-12-26 21:56:24 -0800

    finally 4

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
+    ints = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
-        except SyntaxError as error:
-            print("error")
+            print("{:d}".format(my_list[i]), end="")
+            ints += 1
+        except (ValueError, TypeError):
+            continue
     print()
-    return x
+    return ints
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+
 def safe_print_division(a, b):
-
     try:
-        response = a / b
-    except ZeroDivisionError:
-        response = None
+        c = a / b
+    except (ZeroDivisionError, NameError) as ex:
+        c = None
     finally:
-        print('Inside result: {}'.format(response))
-
-    return response
+        if c is None:
+            print("Inside result: {}".format(c))
+            return c
+        else:
+            print("Inside result: {}".format(c))
+            return c
