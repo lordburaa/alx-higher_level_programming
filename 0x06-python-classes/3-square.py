c411a12d5365b79862087378078d58ffbe85ae70
lordburaa <comrdebiruk97@gmail.com>
2023-12-27 23:59:44 -0800

    Area cheking

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""empty square"""
+"""empty class"""
 
 
 class Square:
-    """Represent square"""
+    """Empty clas"""
+
     def __init__(self, size=0):
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        else:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
 
     def area(self):
-        """calculate area
-        return the sqare of the input"""
-        return (self.__size) ** 2
+        return self.__size * self.__size
