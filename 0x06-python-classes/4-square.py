5f87498839a2caefe4d773db101adcd5dba83c86
lordburaa <comrdebiruk97@gmail.com>
2023-12-28 04:13:30 -0800

    all

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -20,6 +20,8 @@ class Square:
         """
         if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be an integer")
         else:
             return self.__size ** 2
     
