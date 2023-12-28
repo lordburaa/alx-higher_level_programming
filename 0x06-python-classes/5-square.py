0adc9985db9ba1f04e28e54c979867293c36b038
lordburaa <comrdebiruk97@gmail.com>
2023-12-28 04:45:32 -0800

    checking the #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""empty class"""
+
+
+class Square:
+    """Empty clas"""
+
+    def __init__(self, size=0):
+        """Intialize """
+        """if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:"""
+        self.__size = size
+
+    def area(self):
+        """findig the area
+         return the square of size
+        """
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        elif (self.__size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            return self.__size ** 2
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for i in range(self.__size):
+                    print('#', end="")
+                print()
+    @property
+    def size(self):
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        elif (self.__size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set a new value"""
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        elif (self.__size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
