d830e9b696443632bb2cde709338ef3741b0662b
lordburaa <comrdebiruk97@gmail.com>
2023-12-28 04:05:38 -0800

    all

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,7 +13,8 @@ class Square:
             raise ValueError("size must be >= 0")
         
         
-        self.__size = size
+        else:
+            self.__size = size
 
     def area(self):
         """findig the area
