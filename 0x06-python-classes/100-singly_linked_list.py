cbb0029112c108c6703b18fbb3532cebbd850ae1
lordburaa <comrdebiruk97@gmail.com>
2024-01-01 17:46:47 -0800

    oringla

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -57,7 +57,7 @@ class SinglyLinkedList():
                 sll_str += str(node.data) + '\n'
                 node = node.next_node
 
-        return sll_str[0:]
+        return sll_str[0:-1]
 
     def sorted_insert(self, value):
         node = self.__head
