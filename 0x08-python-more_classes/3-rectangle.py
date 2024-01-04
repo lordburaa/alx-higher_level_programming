75eb81fb6d3c8c94dae0166e56f9c6990c36e074
lordburaa <comrdebiruk97@gmail.com>
2024-01-04 01:54:12 -0800

    fix 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,44 +1,65 @@
+#!/usr/bin/python3
 """Class created """
+
 
 class Rectangle:
     """Empty rectangle"""
-    
+
     def __init__(self, width=0, height=0):
-        self.height = height
-        self.width = width
-    
+        """Initializeation of widht and height"""
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
     def __str__(self):
-        c = ""
+        string = ""
         for i in range(self.__height):
             for _ in range(self.__width):
-                c += '#'
-            if (i != self.__height - 1):
-                c += '\n'
-        return c
-
+                string += '#'
+            if i != self.__height -1:
+                string += '\n'
+        return string
 
     def perimeter(self):
-        return 2*(self.__height + self.__width)
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
+
     def area(self):
         return self.__height * self.__width
 
     @property
     def width(self):
-        return self.width
-    
+        """Return width"""
+        return self.__width
+
     @property
     def height(self):
-        return self.height
+        """Return height"""
+        return self.__height
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int) or value < 0:
-            raise TypeError("width must be >= 0")
+        """setting width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int) or value < 0:
-            raise TypeError("height must be >= 0")
+        """seting height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
-
