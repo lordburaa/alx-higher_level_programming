6316344f445f8bb389fc4f93bb3fb82274dde26b
lordburaa <comrdebiruk97@gmail.com>
2024-01-08 12:48:02 -0800

    adding init

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -11,8 +11,10 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """validator"""
+        self.name = name
+        self.value = value
 
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+        if type(self.value) != int:
+            raise TypeError("{} must be an integer".format(self.name))
+        if self.value <= 0:
+            raise ValueError("{} must be greater than 0".format(self.name))
--- 0x0A-python-inheritance/7-main.py
+++ 0x0A-python-inheritance/7-main.py
@@ -3,8 +3,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 bg = BaseGeometry()
 
-bg.integer_validator("my_int", "bb")
-exit()
+bg.integer_validator("my_int", 98)
 bg.integer_validator("width", 89)
 try:
     bg.integer_validator("name", "John")
