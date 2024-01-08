8200adbc0ccbe6881fc010ba582fc46c9e96f352
lordburaa <comrdebiruk97@gmail.com>
2024-01-08 04:20:09 -0800

    fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""Cheking the sorted list"""
+
 
 class MyList(list):
+    """creating list"""
+
     def __init__(self):
         super().__init__()
 
     def print_sorted(self):
+        """printing th esorted list"""
+
         print(sorted(self))
