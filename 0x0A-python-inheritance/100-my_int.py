288eb1364c53a58c47ce6e836ddc6ddc435adaae
lordburaa <comrdebiruk97@gmail.com>
2024-01-08 13:08:52 -0800

    cheking

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""creating myInt class"""
+
+
+class MyInt(int):
+    """creating my int class 
+        
+        checking method """
+    def __init__(self, value):
+        self.value = value
+
+    def __eq__(self, value):
+        return False
+
+    def __ne__(self, value):
+        return True
