b04ded28324ee150b1a933b67e4ef5f752150016
lordburaa <comrdebiruk97@gmail.com>
2024-01-08 11:34:54 -0800

    all

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,15 +10,10 @@ def add_integer(a, b=98):
     """doc
         Cheking the addition and change to int
     """
-    flag = 0
+
     if not isinstance(a, (int, float)) or a == None:
-        flag = 1
         raise ValueError("a must be an integer")
-    else:
-        a = int(a)
     if not isinstance(b, (int , float)) or b == None:
         raise ValueError("b must be an integer")
-    else:
-        b = int(b)
-        if (flag == 0):
-            return a + b
+    
+    return int(a) + int(b)
