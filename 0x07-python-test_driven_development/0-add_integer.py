ff2d86331eec6bd2670d2a6b67606099ddb5d715
lordburaa <comrdebiruk97@gmail.com>
2024-01-08 13:59:18 -0800

    chck

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,11 +9,7 @@
 def add_integer(a, b=98):
     """doc
         Cheking the addition and change to int
-        
-        Args:
-            a first argument
-            b second argumet
-    """
+    """        
 
     if not isinstance(a, (int, float)) or a == None:
         raise ValueError("a must be an integer")
