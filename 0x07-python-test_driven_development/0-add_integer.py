49fcb97562dcf4bb1ca4148e9d0269ca5f31129b
lordburaa <comrdebiruk97@gmail.com>
2024-01-08 14:03:23 -0800

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""new add
-    integer two number
-    two argument 
-    must be an integer
 """
+
+    This module has one functon 
+
+"""
+
 
 
 def add_integer(a, b=98):
     """doc
         Cheking the addition and change to int
     """        
-
     if not isinstance(a, (int, float)) or a == None:
         raise ValueError("a must be an integer")
     if not isinstance(b, (int , float)) or b == None:
