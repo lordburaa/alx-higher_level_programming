2fba636273eb639146ce423c16511c3d8ad61cc4
lordburaa <comrdebiruk97@gmail.com>
2024-01-08 15:27:34 -0800

    fix

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -9,10 +9,7 @@ def print_square(size):
         print square
     """
     if type(size) != int:
-        if isinstance(size, float) and size < 0:
-            raise TypeError("size must be an integer")
-        else:
-            raise TypeError("size must be an integer")
+        raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
 
