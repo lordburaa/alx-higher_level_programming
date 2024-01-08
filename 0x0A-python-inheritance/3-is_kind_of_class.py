32eb57699c64c464657600dee0f7f4f330087d39
lordburaa <comrdebiruk97@gmail.com>
2024-01-08 08:54:49 -0800

    3-is_kind_of_class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""same class or inherit from"""
+
+
+def is_kind_of_class(obj, a_class):
+    """if the object is an instance of a class
+    that inherite from inherit return true"""
+
+    if issubclass(obj, a_class):
+        return True
+    return False
