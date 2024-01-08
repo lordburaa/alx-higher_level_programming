f4d83a8f123cd1836ef099d2c898544b8da49eaa
lordburaa <comrdebiruk97@gmail.com>
2024-01-08 12:19:35 -0800

    all

--- 0x0A-python-inheritance/7-main.py
+++ 0x0A-python-inheritance/7-main.py
@@ -3,8 +3,8 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 bg = BaseGeometry()
 
-bg.integer_validator("my_int", 12)
-
+bg.integer_validator("my_int", -5)
+exit()
 bg.integer_validator("width", 89)
 try:
     bg.integer_validator("name", "John")
