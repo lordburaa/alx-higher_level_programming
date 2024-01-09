9079f848291c805ae97524184ded4d13c2b0b822
lordburaa <comrdebiruk97@gmail.com>
2024-01-09 09:02:00 -0800

    fix

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -10,5 +10,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """ daving json format """
 
-    with open(filename, 'a+', encoding="utf-8") as f:
+    with open(filename, 'w+', encoding="utf-8") as f:
         json.dump(my_obj, f)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,16 +3,15 @@
 
 
 import sys
-import json
 import os.path
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-new = []
 filename = "add_item.json"
-if os.path.isfile(filename):
-    load_from_json_file('add_item.json')
-
+try:
+    new = load_from_json_file(filename)
+except FileNotFoundError:
+    new = []
 new.extend(sys.argv[1:])
 save_to_json_file(new, filename)
