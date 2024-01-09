418821f13cdf1997eb065340560dca38f2cee361
lordburaa <comrdebiruk97@gmail.com>
2024-01-09 04:26:27 -0800

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,10 +8,9 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 new = sys.argv[1:]
-with open('add_item.json', 'r') as r:
 
-    save_to_json_file(new, 'add_item.json')
+save_to_json_file(new, 'add_item.json')
 
-    pr = load_from_json_file('add_item.json')
+pr = load_from_json_file('add_item.json')
 
-    print(pr)
+print(pr)
