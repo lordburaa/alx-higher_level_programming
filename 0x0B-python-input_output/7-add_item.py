4344972e5ab281e84b27624ea906255bf2cb6ee6
lordburaa <comrdebiruk97@gmail.com>
2024-01-09 06:08:14 -0800

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,7 +3,7 @@
 
 
 import sys
-def __name__ == "__main__":
+if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
