b343bf6b9bb28daa1bef7945dbde30d8209d05e5
lordburaa <comrdebiruk97@gmail.com>
2024-01-09 06:06:38 -0800

    all

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,13 +3,13 @@
 
 
 import sys
+def __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    new = sys.argv[1:]
 
-new = sys.argv[1:]
+    new = load_from_json_file('add_item.json')
+    new += sys.argv[1:]
 
-new = load_from_json_file('add_item.json')
-new += sys.argv[1:]
-
-save_to_json_file(new, 'add_item.json')
+    save_to_json_file(new, 'add_item.json')
