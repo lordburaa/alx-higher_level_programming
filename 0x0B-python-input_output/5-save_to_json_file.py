13e6e6d8691d642520c33e88a2978da7aae6ee08
lordburaa <comrdebiruk97@gmail.com>
2024-01-09 05:01:07 -0800

    5-save

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -10,7 +10,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """ daving json format """
 
-    form = json.dumps(my_obj)
-
     with open(filename, 'w+', encoding="utf-8") as f:
-        return f.write(form)
+        json.dump(my_obj, f)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,8 +9,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 new = sys.argv[1:]
 
+for i in range(len(sys.argv) - 1):
+    with open('add_item.json', 'a') as f:
+        f.write(sys.argv[1+i])
 save_to_json_file(new, 'add_item.json')
 
-pr = load_from_json_file('add_item.json')
+load_from_json_file('add_item.json')
 
-print(pr)
