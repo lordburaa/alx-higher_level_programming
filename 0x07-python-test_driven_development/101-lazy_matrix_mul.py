1fbb61499efe5618b679e90f389de5f9554c974c
lordburaa <comrdebiruk97@gmail.com>
2024-01-10 15:09:05 -0800

    new

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -2,11 +2,12 @@
 """ using numpy """
 
 
-
 import numpy as np
+
+
 def lazy_matrix_mul(m_a, m_b):
-    """multiplication of two array 
-        
+    """multiplication of two array
+
         Args:
             m_a - matrix to be multiplied
             m_b - matrix to be multiplied
