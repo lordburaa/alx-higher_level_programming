98e75e2b7ea57b28a0213ff477fcc058c40c4f97
lordburaa <comrdebiruk97@gmail.com>
2024-01-09 23:56:37 -0800

    doc

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,7 +5,10 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+    """class checking the list  to print the max number"""
+
     def checking_integer(self):
+        """checking the function """
         listt = [1 ,2, 3, 6]
         self.assertEqual(max_integer(listt), 6)
         self.assertRaises(TypeError, max_integer, [1,2,3,'hello'])
