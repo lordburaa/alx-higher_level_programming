04a353c41c03d0f8bc29b168dc6792586cb51262
lordburaa <comrdebiruk97@gmail.com>
2024-01-10 00:46:15 -0800

    cheking the doc

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,9 +6,8 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     """class checking the list  to print the max number"""
+    def test_module_ocstring(self):
+            """testing the module docstring"""
+            m = __import__('6-max_integer').__doc__
+            self.assertTrue(len(m) * 1)
 
-#    def checking_integer(self):
-#        """checking the function """
-    listt = [1 ,2, 3, 6]
-#   assertEqual(max_integer(listt), 6)
-    assert max_integer(listt) == 6
