b9d93dff0c4074fa5937197f3a5e27a0b0ba75c7
lordburaa <comrdebiruk97@gmail.com>
2024-01-09 16:49:11 -0800

    fix

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" creating class """
+
+
+class Student:
+    """student """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Method that returns direcotry description """
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
+
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+
+            new_dict = {}
+
+            for attr in range(len(attrs)):
+                for satr in obj:
+                    if attrs[attr] == satr:
+                        new_dict[satr] = obj[satr]
+            return new_dict
+        return obj
