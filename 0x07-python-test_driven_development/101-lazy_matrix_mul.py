8960684e937889279097c550405307dc280238f9
lordburaa <comrdebiruk97@gmail.com>
2024-01-10 06:23:02 -0800

    fix

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" using numpy """
+
+
+
+import numpy as np
+def lazy_matrix_mul(m_a, m_b):
+    """multiplication of two array 
+        
+        Args:
+            m_a - matrix to be multiplied
+            m_b - matrix to be multiplied
+
+    """
+    return np.matmul(m_a, m_b)
