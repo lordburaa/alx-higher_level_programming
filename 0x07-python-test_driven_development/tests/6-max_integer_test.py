26fad0c5b821988332982ec8c9992361c729d798
lordburaa <comrdebiruk97@gmail.com>
2024-01-10 01:15:47 -0800

    fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,10 +10,14 @@ class TestMaxInteger(unittest.TestCase):
             """testing the module docstring"""
             m = __import__('6-max_integer').__doc__
             self.assertTrue(len(m) * 1)
-    def test_list(self):
+    def test_listt(self):
         """cheking the list"""
         
-        self.assertEqual(len([]), 0)
+        self.assertNotEqual(len([]), 5)
+
+    def test_maxx(self):
+       """checking the list"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
 if __name__ == '__main__':
     unittest.main()
