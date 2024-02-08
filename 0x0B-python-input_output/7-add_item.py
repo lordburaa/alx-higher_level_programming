2264d1b5051348b9ce22815e44077f0a39d40aec
lordburaa <comrdebiruk97@gmail.com>
2024-02-08 04:05:24 -0800

    not right

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,16 +3,19 @@
     json
 """
 import json
+import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
-def add_item(*args):
+def add_item(args):
     """add all argument to a pyton list and save them to a file"""
-
-    filename="add_item.json"
-    l = load_from_json_file(filename)
+    list_t=[]
+    list_t=list(load_from_json_file("add_item.json"))
     for i in args:
-        l.append(i)
-
-    save_to_file(l, filename)
+        list_t.append(i)
+    new = json.dumps(list_t)
+    save_to_json_file(new, "add_item.json")
+    print("item added succesfully")
+args = sys.argv[1:]
+add_item(args)
