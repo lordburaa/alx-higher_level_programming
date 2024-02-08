1fbfe512688ff41cfc434b12f2e9ee480b41ad0f
lordburaa <comrdebiruk97@gmail.com>
2024-02-08 00:06:39 -0800

    add doc

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,6 +7,7 @@ import json
 
 
 def load_from_json_file(filename):
+    """create an object from  JSON  file"""
     with open(filename) as f:
         new_str = json.load(f)
         return new_str
