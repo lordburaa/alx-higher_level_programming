4fd0aff21e0adeacb7b25f0f9a42febf43adae5c
lordburaa <comrdebiruk97@gmail.com>
2024-02-08 00:05:04 -0800

    changing answer

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-""" json """
-
+"""
+    json
+"""
 
 import json
 
 
 def load_from_json_file(filename):
-    """ loading form the file decode it"""
-
-    with open(filename, 'r', encoding="utf-8") as f:
-        new = f.read()
-        objec = json.loads(new)
-        return objec
+    with open(filename) as f:
+        new_str = json.load(f)
+        return new_str
