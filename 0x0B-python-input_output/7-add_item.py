cc50298d9fa444bcd935b63e455c5fb85995c69f
lordburaa <comrdebiruk97@gmail.com>
2024-02-08 00:36:35 -0800

    new value

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-""" scripts for the json """
-
-
-import sys
-import os.path
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+"""
+    json
+"""
+import json
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-filename = "add_item.json"
-try:
-    new = load_from_json_file(filename)
-except FileNotFoundError:
-    new = []
-new.extend(sys.argv[1:])
-save_to_json_file(new, filename)
+
+def add_item(filename, *args):
+    """add all argument to a pyton list and save them to a file"""
+
+    filename="add_item.json"
+    l = load_from_json_file
+    for i in args:
+        l.append(i)
+
+    save_to_file(l, filename)
