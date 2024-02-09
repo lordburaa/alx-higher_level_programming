72358504a3f38aa7de5aecb2f190ba4ee73ccc56
lordburaa <comrdebiruk97@gmail.com>
2024-02-08 22:40:49 -0800

    error passed?

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""
-    json
-"""
-import json
+""" scripts for the json """
+
+
 import sys
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import json
+import os.path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-def add_item(args):
-    """add all argument to a pyton list and save them to a file"""
-    list_t=[]
-    list_t=list(load_from_json_file("add_item.json"))
-    for i in args:
-        list_t.append(i)
-    new = json.dumps(list_t)
-    save_to_json_file(new, "add_item.json")
-    print("item added succesfully")
-args = sys.argv[1:]
-add_item(args)
+filename = "add_item.json"
+if os.path.isfile(filename):
+    new = load_from_json_file(filename)
+else:
+    new = []
+new.extend(sys.argv[1:])
+save_to_json_file(new, filename)
