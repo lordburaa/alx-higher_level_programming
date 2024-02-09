3229d7c22363a0533a0b6baf678eb16b0b071bdb
lordburaa <comrdebiruk97@gmail.com>
2024-02-09 00:52:55 -0800

    changing the answe

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-""" creating class """
+"""
+    create class
+"""
 
 
 class Student:
-    """student """
-
+    """class student"""
     def __init__(self, first_name, last_name, age):
+        """instantiation"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """to json method"""
         return self.__dict__
