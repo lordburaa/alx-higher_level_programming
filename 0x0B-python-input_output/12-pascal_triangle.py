eb82644aaea3899b17a9921014d6247e017dc44a
lordburaa <comrdebiruk97@gmail.com>
2024-02-10 00:42:49 -0800

    add task

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    """pascal triangle"""
+    tmp = [0]
+    pascal_tr = []
+
+    for i in range(1, n+1):
+        tmp = add(tmp, i)
+        pascal_tr.append(tmp)
+
+        if i == 6:
+            return pascal_tr
+
+    return pascal_tr
+
+
+def add(old, n):
+    """add the value side to each other  """
+    list_new = []
+    summ = 0
+    if len(old) == 1:
+        old = [1, 1]
+    else:
+        for i in range(len(old) - 1):
+            summ = old[i] + old[i + 1]
+            list_new.append(summ)
+
+    list_new.insert(0, 1)
+
+    if n != 1:
+        list_new.append(1)
+
+    return list_new
