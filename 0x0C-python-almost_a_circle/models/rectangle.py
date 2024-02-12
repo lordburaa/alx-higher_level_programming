8e33a8a95702d0a51e87339c176eef560ef538d3
lordburaa <comrdebiruk97@gmail.com>
2024-02-12 00:34:02 -0800

    fix pycodestyle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,36 +14,45 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
         super().__init__(id)
-    #Getter method
+
+    # Getter method
     @property
     def width(self):
+        """width attribute"""
         return self.__width
-    
+
     @property
     def height(self):
+        """height getter method"""
         return self.__height
-    
+
     @property
     def x(self):
+        """x getter method"""
         return self.__x
-    
+
     @property
     def y(self):
+        """y getter method"""
         return self.__y
 
+    # setter method for attribute
     @setter.width
-    def width(self, width)
+    def width(self, width):
+        """width setter method"""
         self.__width = width
-    
+
     @setter.height
-    def height(self, height)
+    def height(self, height):
+        """height setter method"""
         self.__height = height
-    
+
     @setter.x
-    def x(self, x)
+    def x(self, x):
+        """x setter method"""
         self.__x = x
 
-
     @setter.y
-    def y(self, y)
+    def y(self, y):
+        """y setter method"""
         self.__y = y
