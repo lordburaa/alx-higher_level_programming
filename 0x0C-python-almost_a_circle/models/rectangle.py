5cf1000ac1961472cc2b374eddedcb793cb8d3d5
lordburaa <comrdebiruk97@gmail.com>
2024-02-12 04:07:13 -0800

    removing the private

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,10 +9,10 @@ class Rectangle(Base):
     """rectangle class  inherit `Base` class"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """init constructor"""
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     # Getter method
@@ -40,35 +40,35 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """width setter method"""
-        if not is isinstance(width, int):
-            raise TypeError(f"{width} must be an integer")
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
         if width <= 0:
-            raise ValueError(f"{width} must be > 0")
+            raise ValueError("width must be > 0")
         self.__width = width
 
     @height.setter
     def height(self, height):
         """height setter method"""
-        if not is isinstance(height, int):
-            raise TypeError(f"{height} must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
         if height <= 0:
-            raise ValueError(f"{height} must be > 0")
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @x.setter
     def x(self, x):
         """x setter method"""
-        if not is isinstance(x, int):
-            raise TypeError(f"{x} must be an integer")
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
         if x < 0:
-            raise ValueError(f"{x} must be >= 0")
+            raise ValueError("x must be >= 0")
         self.__x = x
 
     @y.setter
     def y(self, y):
         """y setter method"""
-        if not is isinstance(y, int):
-            raise TypeError(f"{y} must be an integer")
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
         if y < 0:
-            raise ValueError(f"{y} must be >= 0")
+            raise ValueError("y must be >= 0")
         self.__y = y
