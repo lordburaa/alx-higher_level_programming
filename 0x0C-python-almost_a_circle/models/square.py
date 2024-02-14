9f5b4092d5536c1d2cb6d4fecfce33d4e084ffba
lordburaa <comrdebiruk97@gmail.com>
2024-02-13 23:53:55 -0800

    addin id

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,15 +8,12 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """creating square class that inherit the Rectangle class"""
 
-    nc_class = 0
-
     def __init__(self, size, x=0, y=0, id=None):
         """instantiation of the child class inherit class Rectangle"""
         super().__init__(size, size, x, y, id)
-        Square.nc_class += 1
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({Square.nc_class}) {super().x}/{super().y} - {super().width}"
+        return f"[{self.__class__.__name__}] ({super().id}) {super().x}/{super().y} - {super().width}"
 
 #    @property
 #    def size(self):
