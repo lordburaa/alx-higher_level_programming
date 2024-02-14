0405c344744584ee37078d072067550bc65faa1a
lordburaa <comrdebiruk97@gmail.com>
2024-02-13 22:55:15 -0800

    fix Square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    create class
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """creating square class that inherit the Rectangle class"""
+    nc_class = 0
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+        self.size = size
+        Square.nc_class += 1
+
+    def __str__(self):
+        return f"{Square.__name__} ({Square.nc_class}) {self.x}/{self.y} - {self.size}"
+
