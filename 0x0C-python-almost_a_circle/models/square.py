ff5c6d970be797c03d95bedc8f87f17a6f075d56
lordburaa <comrdebiruk97@gmail.com>
2024-02-13 23:17:46 -0800

    fix set and getter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,9 +10,16 @@ class Square(Rectangle):
     nc_class = 0
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        self.size = size
         Square.nc_class += 1
-        self.width, self.height = size
 
     def __str__(self):
         return f"[Square] ({Square.nc_class}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        self.width = size
+        self.height = size
