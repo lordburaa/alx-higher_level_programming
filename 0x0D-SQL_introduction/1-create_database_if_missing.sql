d98a4e8f0fc0a26646f5ae5f0a65080dfb90250f
lordburaa <comrdebiruk97@gmail.com>
2024-03-14 05:50:51 -0700

    adding new format

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,9 @@
 -- creates database `hbtn_0c_0`
-CREATE DATABASE `hbtn_0c_0`;
+IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE name=`hbtn_0c_0`)
+BEGIN
+	CREATE DATABASE `hbtn_0c_0`;
+END
+ELSE
+BEGIN
+
+END;
