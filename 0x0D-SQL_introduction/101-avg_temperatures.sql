317439f6a577735c03e0667a9563ea72bf8fd6eb
lordburaa <comrdebiruk97@gmail.com>
2024-03-15 05:39:58 -0700

    retrieve only the temp

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- displays teh average tempereature(Fahrenheurt( by city ordered by temprature descending)
-SELECT `city`, AVG(`value`) AS avg_temp FROM `temperatures` GROUP BY `city` ORDER BY avg_value DESC;
+SELECT AVG(`value`) AS avg_temp FROM `temperatures` GROUP BY `city` ORDER BY avg_temp DESC;
