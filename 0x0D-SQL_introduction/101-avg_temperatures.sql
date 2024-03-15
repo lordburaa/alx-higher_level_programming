1b62702a0d67941b0a17535f02dfa909b39c613f
lordburaa <comrdebiruk97@gmail.com>
2024-03-15 05:26:41 -0700

    adding temp

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- displays teh average tempereature(Fahrenheurt( by city ordered by temprature descending)
-SELECT `city`, AVG(`value`) as avg_value FROM `temperatures` GROUP BY `city` ORDER BY avg_value DESC;
+SELECT `city`, AVG(`value`) as avg_temp FROM `temperatures` GROUP BY `city` ORDER BY avg_value DESC;
