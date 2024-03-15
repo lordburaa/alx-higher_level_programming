9915ca64fc65b170e474f2fc3cc62fda78bfb1ce
lordburaa <comrdebiruk97@gmail.com>
2024-03-15 04:44:23 -0700

    changin

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,4 +2,5 @@
 USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE second_table CONVERT TO CHARACTER SET utf8mb4 COLLATE  utf8mb4_unicode_ci;
+ALTER TABLE second_table MODIFY name  VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+SET names utf8;
