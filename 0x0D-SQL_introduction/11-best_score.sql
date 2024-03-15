19805899e80127b7eae186ac71ede731d899b449
lordburaa <comrdebiruk97@gmail.com>
2024-03-15 00:28:53 -0700

    adding en w

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- lists all records score >= 10 in the table 
-SELECT score, name FROM secont_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
