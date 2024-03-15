3664e540cc92978b0e9fe1c36862dca2c6c51bdb
lordburaa <comrdebiruk97@gmail.com>
2024-03-15 04:14:42 -0700

    adding got utf

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,5 @@
+-- converts hbtn_0c_0 DATABASE to UTF8
+USE hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE second_table CONVERT TO CHARACTER SET utf8mb4 COLLATE  utf8mb4_unicode_ci;
