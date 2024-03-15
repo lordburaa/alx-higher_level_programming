dec917645818462790bb50cafe522a8005bfa88a
lordburaa <comrdebiruk97@gmail.com>
2024-03-15 05:25:24 -0700

    fix

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,2 @@
 -- displays teh average tempereature(Fahrenheurt( by city ordered by temprature descending)
-
 SELECT `city`, AVG(`value`) as avg_value FROM `temperatures` GROUP BY `city` ORDER BY avg_value DESC;
