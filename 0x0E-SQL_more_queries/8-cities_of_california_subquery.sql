6b2313f0f6ceead42ef252e61d073458fe1fa88a
lordburaa <comrdebiruk97@gmail.com>
2024-03-19 01:58:30 -0700

    adding the rimary key and foreign key

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- lists all the cities of california that can be found in the database hbtn_0d_usa
 
-SELECT cities.id, cities.name FROM cities, states WHERE states.name='California';
+SELECT cities.id, cities.name FROM cities, states WHERE cities.state_id = states.id AND states.name='California';
