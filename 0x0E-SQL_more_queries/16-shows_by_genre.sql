3efad382bce48bc3c2daab07b0668ba487c64c7b
lordburaa <comrdebiruk97@gmail.com>
2024-03-19 05:50:37 -0700

    list all shows and generes

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,3 @@
+-- lists all shows and genres linked to that fron the database hbtn_0d_tvshows
+
+SELECT s.title, g.name FROM tv_shows s JOIN tv_show_genres sg  ON sg.show_id=s.id JOIN tv_genres g ON g.id=sg.genre_id  ORDER BY s.title, g.name
