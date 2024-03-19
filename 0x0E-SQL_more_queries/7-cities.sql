a32f1b067a85bf08adb703fbe3142c76eaacc555
lordburaa <comrdebiruk97@gmail.com>
2024-03-19 00:47:34 -0700

    adding foregin key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,5 +7,5 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	UNIQUE KEY id_unique(id),
-	FOREIGN KEY (state_id) REFERENCES state(id)
+	FOREIGN KEY (state_id) REFERENCES states(id)
 	);
