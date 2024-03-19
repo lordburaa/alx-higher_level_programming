b716f6c935a0cdf581a0a211ee9197970cf93132
lordburaa <comrdebiruk97@gmail.com>
2024-03-19 04:10:10 -0700

    fix red flag

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- lists all shows contained in hbtn_od_tvshows that have at least one genr likned
 
-SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres WHERE tv_show_genres.genre_id = tv_shows.id or tv_show_genres.show_id = tv_shows.id ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres WHERE tv_show_genres.show_id=tv_shows.id ORDER BY tv_shows.title, tv_show_genres.genre_id;
