c09ec97166d392a123658c7ce13eccd8f699ddbe
lordburaa <comrdebiruk97@gmail.com>
2024-03-19 06:58:41 -0700

    not my genre

--- 100-not_my_genres.sql
+++ 100-not_my_genres.sql
@@ -2,4 +2,4 @@
 SELECT g.name FROM tv_genres g LEFT JOIN tv_show_genres sg ON sg.genre_id=g.id
 WHERE g.name NOT IN (SELECT DISTINCT t.name FROM tv_genres t JOIN tv_show_genres sg  ON sg.genre_id=t.id
 	JOIN tv_shows s ON s.id=sg.show_id  WHERE s.title='Dexter' GROUP BY t.name) 
-GROUP BY g.name ORDER BY g.name;
+ORDER BY g.name;
