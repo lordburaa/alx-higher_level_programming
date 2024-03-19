bd47357b504ba71a6c6a5a21415be829c9a145d9
lordburaa <comrdebiruk97@gmail.com>
2024-03-19 05:40:07 -0700

    only comdey

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,2 +1,2 @@
 -- lists all comedy show in the databse hbtn_od_tvshows
-SELECT s.title FROM tv_shows s JOIN  tv_show_genres sg ON s.id=sg.show_id  JOIN tv_genres g ON g.id=sg.genre_id WHERE g.name='Comedy' ORDER BY s.name;
+SELECT s.title FROM tv_shows s JOIN  tv_show_genres sg ON s.id=sg.show_id  JOIN tv_genres g ON g.id=sg.genre_id WHERE g.name='Comedy' ORDER BY s.title;
