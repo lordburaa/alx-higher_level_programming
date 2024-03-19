2fca3db6567e318280af3fec1d1f9443405264e1
lordburaa <comrdebiruk97@gmail.com>
2024-03-19 01:38:01 -0700

    change to state id

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,3 @@
 -- lists all the cities of california that can be found in the database hbtn_0d_usa
 
-SELECT cities.id, cities.state_id, cities.name FROM cities, states WHERE states.name='California';
-
+SELECT cities.id, states.id, cities.name FROM cities, states WHERE states.id=1;
