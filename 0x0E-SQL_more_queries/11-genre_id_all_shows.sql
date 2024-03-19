8ccee60523dc69491113b74b35153c8c3a1df657
lordburaa <comrdebiruk97@gmail.com>
2024-03-19 04:37:51 -0700

    adding null

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,2 @@
 -- lists all shows contained in the databse hbtn_0d_tvshows
-SELECT s.title, g.genre_id FROM tv_shows s, tv_show_genres g  WHERE s.id=g.show_id ORDER BY s.title, g.genre_id;
+SELECT s.title, g.genre_id FROM tv_shows s LEFT JOIN tv_show_genres g ON s.id=g.show_id ORDER BY s.title, g.genre_id;
