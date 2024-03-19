4caa93bd4ecac9137cddc724a3ac02b5ff8a30e3
lordburaa <comrdebiruk97@gmail.com>
2024-03-18 23:47:06 -0700

    ading the script

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- create teh databse hbtn_0c_2 and the user user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
-CREATE USER IF NOT EXISTS 'user_0d_2'@'loacalhost' IDENTIFIED BY 'user_0d_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
