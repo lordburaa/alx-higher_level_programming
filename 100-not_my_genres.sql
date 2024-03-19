c5a61fb22fa748f26b6a4fcfb37780acbdb5f7fe
lordburaa <comrdebiruk97@gmail.com>
2024-03-19 06:57:48 -0700

    addign

--- 100-not_my_genres.sql
+++ 100-not_my_genres.sql
@@ -1,5 +1,5 @@
 -- list all genres not linked to the show 
 SELECT g.name FROM tv_genres g LEFT JOIN tv_show_genres sg ON sg.genre_id=g.id
 WHERE g.name NOT IN (SELECT DISTINCT t.name FROM tv_genres t JOIN tv_show_genres sg  ON sg.genre_id=t.id
-	JOIN tv_shows s ON s.id=sg.show_id  WHERE s.title='Dexter') 
+	JOIN tv_shows s ON s.id=sg.show_id  WHERE s.title='Dexter' GROUP BY t.name) 
 GROUP BY g.name ORDER BY g.name;
