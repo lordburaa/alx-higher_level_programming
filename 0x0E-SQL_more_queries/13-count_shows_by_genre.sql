77fa6419dc8c81c3eac7fc0e1f8e9130034e4bae
lordburaa <comrdebiruk97@gmail.com>
2024-03-19 05:10:23 -0700

    genre id

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- list Number of shows by genre
-SELECT g.name, COUNT(g.name) AS number_of_shows FROM tv_genres g JOIN tv_show_genres sg ON sg.genre_id=g.id GROUP BY g.name ORDER BY number_of_shows DESC
+SELECT g.name, COUNT(g.name) AS number_of_shows FROM tv_genres g JOIN tv_show_genres sg ON sg.genre_id=g.id WHERE sg.genre_id IS NOT NULL GROUP BY g.name ORDER BY number_of_shows DESC
