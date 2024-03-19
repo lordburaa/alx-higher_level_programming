b0f16bf6692d79e7e9e931ca53a9147af80493c1
lordburaa <comrdebiruk97@gmail.com>
2024-03-18 23:49:38 -0700

    adding dic

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,7 @@
--- create teh databse hbtn_0c_2 and the user user_0d_2
+-- create the databse hbtn_0c_2 and
+-- create user user_od_2 if not exists
+-- give permission fo the useruser_0d_2 SELECT 
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_pwd';
