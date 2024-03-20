8d76a1458b0eef94ae929ce4b5e884b5b69bd2f6
lordburaa <comrdebiruk97@gmail.com>
2024-03-20 00:28:27 -0700

    fix

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- list all shows from hbtn_0d_tvshow_rate
-SELECT s.title, sum(r.rate) AS 'rating'
+SELECT s.title, SUM(r.rate) AS 'rating'
 FROM  tv_shows s
-left JOIN tv_show_ratings r on r.show_id=s.id
+JOIN tv_show_ratings r on r.show_id=s.id
 group by s.title
 ORDER BY rating DESC;
