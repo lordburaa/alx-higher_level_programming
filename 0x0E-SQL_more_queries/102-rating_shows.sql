06e49690f537737df764d9829065728fba92bc23
lordburaa <comrdebiruk97@gmail.com>
2024-03-20 00:29:29 -0700

    changing the keyword to upercase

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- list all shows from hbtn_0d_tvshow_rate
 SELECT s.title, SUM(r.rate) AS 'rating'
 FROM  tv_shows s
-JOIN tv_show_ratings r on r.show_id=s.id
-group by s.title
+JOIN tv_show_ratings r ON r.show_id=s.id
+GROUP BY s.title
 ORDER BY rating DESC;
