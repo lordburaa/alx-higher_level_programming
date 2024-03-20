72039eeffe8699914b0c61b13914ea16dc976b03
lordburaa <comrdebiruk97@gmail.com>
2024-03-19 23:43:50 -0700

    adding new

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,8 +1,8 @@
 -- list all shows from hbtn_0d_tvshow_rate
 SELECT s.title, sum(r.rate) AS 'rating'
-FROM tv_shows s 
-JOIN tv_show_genres sg ON s.id=sg.show_id
-JOIN tv_show_ratings r on r.show_id=sg.show_id
-JOIN tv_genres g ON sg.genre_id=g.id
-GROUP BY s.title
+FROM tv_show_genres sg
+right JOIN tv_shows s ON s.id=sg.show_id
+left JOIN tv_show_ratings r on r.show_id=s.id
+left JOIN tv_genres g ON sg.genre_id=g.id
+GROUP BY s.id
 ORDER BY rating DESC;
