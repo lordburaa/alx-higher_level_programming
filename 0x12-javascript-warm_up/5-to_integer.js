511f8d9ef3ad53933e73490fb9dfca2f2afefd03
lordburaa <comrdebiruk97@gmail.com>
2024-03-29 05:08:40 -0700

    check Pass

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+const num = parseInt(argv[2]);
+
+if (Number.isNaN(num)) {
+	console.log('Not a number');
+}
+else {
+	console.log(`My number: ${num}`);
+}
