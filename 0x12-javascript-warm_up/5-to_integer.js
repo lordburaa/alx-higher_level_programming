6495bcbb804c0f8c61a61122b271372b3bb50781
lordburaa <comrdebiruk97@gmail.com>
2024-03-29 05:29:36 -0700

    fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ const { argv } = require('process');
 
 const num = parseInt(argv[2]);
 
-if (Number.isNaN(num)) {
+if (isNaN(num)) {
 	console.log('Not a number');
 } else {
 	console.log(`My number: ${num}`);
