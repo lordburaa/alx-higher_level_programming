5758d594d38a447ce15824c8f029f067f8a964b1
lordburaa <comrdebiruk97@gmail.com>
2024-03-29 04:26:07 -0700

    adding task3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+if (typeof argv[2] === 'undefined') {
+  console.log('No argument')
+} else {
+  for (let i = 2; typeof argv[i] !== 'undefined'; i++) {
+	  console.log(argv[i])
+  }
+}
