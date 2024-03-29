4c837ddc855392fc0745a251d4ca481ae4d12162
lordburaa <comrdebiruk97@gmail.com>
2024-03-29 05:53:02 -0700

    fix error

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -7,7 +7,7 @@ const num = parseInt(argv[2]);
 if (isNaN(num)) {
  console.log('Missing number of occurences');
 } else {
-	for (let i = 0; i < num; i++) {
-		console.log('C is fun');
-	}
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
 }
