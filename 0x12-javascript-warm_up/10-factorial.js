a6a811cb5b1f128603226e3af9e478aa92a29636
lordburaa <comrdebiruk97@gmail.com>
2024-03-29 06:58:57 -0700

    fix the facorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const num = parseInt(argv[2])
+
+function fac (a) {
+  if (a === 1) {
+	  return 1;
+  }
+
+  else {
+    return a*fac(a - 1)
+  }
+}
+console.log(fac(num));
