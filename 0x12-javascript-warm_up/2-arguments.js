ffbcd2f2705fb1abbb489d7800d02f01f94d9a23
lordburaa <comrdebiruk97@gmail.com>
2024-03-29 03:57:36 -0700

    rule

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
+
 const { argv } = require('node:process')
 
 if (argv.length === 2){
 	console.log('No argument')
-}
-else if (argv.length === 3){
+} else if (argv.length === 3){
 	console.log('Argument found')
-}
-else {
+} else {
 	console.log('Arguments found')
 }
