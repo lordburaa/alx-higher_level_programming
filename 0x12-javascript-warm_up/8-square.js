a408220168084e0fcf3f32f967fcaa454d11a0f1
lordburaa <comrdebiruk97@gmail.com>
2024-03-29 06:47:04 -0700

    adding 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,8 +4,8 @@ const num = parseInt(argv[2]);
 if (isNaN(num)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < num; i++) {
-  let shape = 'X'.repeat(num)
-  console.log(shape);
-  }
+    for (let i = 0; i < num; i++) {
+      let shape = 'X'.repeat(num)
+      console.log(shape);
+    }
 }
