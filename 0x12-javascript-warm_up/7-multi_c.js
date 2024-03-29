65dc6cb9eba319f5c2870a6d69c710029b05cd21
lordburaa <comrdebiruk97@gmail.com>
2024-03-29 06:20:24 -0700

    adding new anser>

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -7,7 +7,7 @@ const num = parseInt(argv[2]);
 if (isNaN(num)) {
  console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < argv[2]; i++) {
+  for (let i = 0; i < num; i++) {
     console.log('C is fun');
   }
 }
