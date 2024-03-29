8b8ba1e24f24096176dfcd1227bcb92afa9df08a
lordburaa <comrdebiruk97@gmail.com>
2024-03-29 03:55:29 -0700

    adding conditoin

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const { argv } = require('node:process')
 
-if (argv.length == 2){
+if (argv.length === 2){
 	console.log('No argument')
 }
-else if (argv.length == 3){
+else if (argv.length === 3){
 	console.log('Argument found')
 }
 else {
