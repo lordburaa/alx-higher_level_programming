fbaf2c454a367cf7f56d230417f880f76d67201c
lordburaa <comrdebiruk97@gmail.com>
2024-03-29 06:04:02 -0700

    error style

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ const { argv } = require('process');
 const num = parseInt(argv[2]);
 
 if (isNaN(num)) {
- console.log('Missing number of occurences');
+ console.log('Missing number of occurrences');
 } else {
     for (let i = 0; i < num; i++) {
       console.log('C is fun');
