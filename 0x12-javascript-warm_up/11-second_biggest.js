7fced7b18cadf494750009109977e484fe9c2c61
lordburaa <comrdebiruk97@gmail.com>
2024-03-31 23:50:42 -0700

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,17 +4,19 @@ const { argv } = require('process')
 const array = []
 
 for (let i = 2; i < argv.length; i++) {
-	array.push(parseInt(argv[i]));
+  array.push(parseInt(argv[i]));
 }
 const maxx = Math.max.apply(null, array);
 
-let sd_max = 0;
-for (let i = 2; i < argv.length; i++) {
-	if (sd_max !== maxx) {
-		if (sd_max < array[i] && array[i] != maxx) {
-			sd_max = array[i];
-		}
-	}
+let sd_max = Number.MIN_SAFE_INTEGER;
+if (argv.length > 2 ) {
+for (let i = 0; i < array.length; i++) {
+  if (array[i] !== maxx) {
+    if (sd_max < array[i]) {
+      sd_max = array[i];
+    }
+  }
+}
 }
 console.log(sd_max);
 
