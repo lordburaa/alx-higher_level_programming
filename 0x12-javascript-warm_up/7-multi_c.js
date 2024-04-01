1f79702772a47328c1e737cf72f943243743370f
lordburaa <comrdebiruk97@gmail.com>
2024-03-31 22:40:47 -0700

    adding 7-muti_c semicolon

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-const { argv } = require('process')
-let i = 0
+const { argv } = require('process');
+let i = 0;
 
-const num = parseInt(argv[2])
+const num = parseInt(argv[2]);
 
 if (isNaN(num)) {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 } else {
   for (i; i < num; i++) {
-    console.log('C is fun')
+    console.log('C is fun');
   }
 }
