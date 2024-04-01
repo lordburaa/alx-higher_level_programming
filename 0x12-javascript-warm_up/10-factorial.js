cf215e2723f621bdb44015b0bf5237c9f9ea94b9
lordburaa <comrdebiruk97@gmail.com>
2024-03-31 22:44:41 -0700

    adding seomiconon

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-const { argv } = require('process')
-const num = parseInt(argv[2])
+const { argv } = require('process');
+const num = parseInt(argv[2]);
 
 function fac (a) {
   if (a === 1 || isNaN(a)) {
-    return 1
+    return 1;
   } else {
-    return a * fac(a - 1)
+    return a * fac(a - 1);
   }
 }
-console.log(fac(num))
+console.log(fac(num));
