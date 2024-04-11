217bb443cf11061eabe9c5e7cf362a6df3abd817
lordburaa <comrdebiruk97@gmail.com>
2024-04-11 02:21:28 -0700

    adding user in mysqlb

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ import MySQLdb
 import sys
 if __name__ == '__main__':
 
-    db = MySQLdb.connect(password=sys.argv[2], database=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
     c = db.cursor()
     c.execute("""SELECT * FROM states ORDER BY states.id ASC""")
     r = c.fetchall()
