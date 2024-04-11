4dc5c851c67c07c1515f6db2068e67b2493e5a91
lordburaa <comrdebiruk97@gmail.com>
2024-04-11 03:27:56 -0700

    1-fileter

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script fileter
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host = 'localhost', user=sys.argv[1], password=sys.argv[2], database=sys.argv[3], port=3306)
+    
+    db_c = db.cursor()
+    db_c.execute("SELECT * FROM states WHERE states.name LIKE 'n%'")
+    row_select=db_c.fetchall()
+
+    for row in row_select:
+        print(row)
