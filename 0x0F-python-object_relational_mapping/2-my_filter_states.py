598b92d91a34340aa81423b12a16d5fad656fe60
lordburaa <comrdebiruk97@gmail.com>
2024-04-11 04:04:46 -0700

    handling the not found in hte database

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Takes an Argument and display all values in the states tables of hbtn_0e_0_usa where name matche
+the argument
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    try:
+
+        db = MySQLdb.connect(host='localhost', user=sys.argv[1], password=sys.argv[2], database=sys.argv[3], port=3306)
+        db_c = db.cursor()
+        db_c.execute("SELECT * FROM states WHERE states.name={} ORDER BY states.id ASC".format(sys.argv[4]))
+        row_select = db_c.fetchall() 
+        for row in row_selecte:
+            print(row)
+    except:
+        pass
+        
