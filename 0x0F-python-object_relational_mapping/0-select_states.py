f0d1f6af4ce220c153f6448d60512c80b55c8a45
lordburaa <comrdebiruk97@gmail.com>
2024-04-11 02:53:11 -0700

    adding doc

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
+"""
+This script liss all states from the databsee 
+"""
 import MySQLdb
 import sys
 if __name__ == '__main__':
-
     db = MySQLdb.connect(host="localhost",
                          user=sys.argv[1], password=sys.argv[2],
                          database=sys.argv[3], port=3306)
