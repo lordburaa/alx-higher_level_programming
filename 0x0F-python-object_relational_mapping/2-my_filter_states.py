fc5f4705b234af6a3814ea2030a4bdeaeb30b861
lordburaa <comrdebiruk97@gmail.com>
2024-04-11 04:38:31 -0700

    adding binary

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
                          database=sys.argv[3], port=3306)
     db_c = db.cursor()
     # avoid the sql injection
-    db_c.execute("SELECT * FROM states WHERE states.name='{}'\
+    db_c.execute("SELECT * FROM states WHERE BINARY states.name='{}'\
             ORDER BY states.id ASC".format(sys.argv[4]))
     row_select = db_c.fetchall()
     for row in row_select:
