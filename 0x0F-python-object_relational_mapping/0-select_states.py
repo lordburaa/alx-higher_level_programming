cbd73522f3ec10136e98b3ea6b0439f6a01c918f
lordburaa <comrdebiruk97@gmail.com>
2024-04-11 03:12:12 -0700

    adding try/except

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,14 +10,18 @@ import sys
 
 if __name__ == '__main__':
     """ doc checked """
-    db = MySQLdb.connect(host="localhost",
-                         user=sys.argv[1], password=sys.argv[2],
-                         database=sys.argv[3], port=3306)
-    c = db.cursor()
-    c.execute("SELECT * FROM states ORDER BY states.id ASC")
-    r = c.fetchall()
+    try:
 
-    for row in r:
-        print(i)
-    c.close()
-    db.close()
+        db = MySQLdb.connect(host="localhost",
+                            user=sys.argv[1], password=sys.argv[2],
+                            database=sys.argv[3], port=3306)
+        c = db.cursor()
+        c.execute("SELECT * FROM states ORDER BY states.id ASC")
+        r = c.fetchall()
+
+        for row in r:
+            print(i)
+        c.close()
+        db.close()
+    except MySQLdb.error as e:
+        print(e)
