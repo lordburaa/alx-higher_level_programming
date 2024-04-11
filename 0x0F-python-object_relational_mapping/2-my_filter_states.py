6ecf5f08d90baf227eed8f6987cb67d84ad09adb
lordburaa <comrdebiruk97@gmail.com>
2024-04-11 04:29:10 -0700

    avod hte sql injection

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,8 +13,9 @@ if __name__ == '__main__':
                          password=sys.argv[2],
                          database=sys.argv[3], port=3306)
     db_c = db.cursor()
-    db_c.execute("SELECT * FROM states WHERE states.name='{}'\
-            ORDER BY states.id ASC".format(sys.argv[4]))
+    # avoid the sql injection
+    db_c.execute("SELECT * FROM states WHERE states.name= %s\
+            ORDER BY states.id ASC", sys.argv[4])
     row_select = db_c.fetchall()
     for row in row_select:
         print(row)
