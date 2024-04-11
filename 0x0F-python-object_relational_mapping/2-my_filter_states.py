4faeff73b4a17abd7e015b3bc40c4eeb27828d11
lordburaa <comrdebiruk97@gmail.com>
2024-04-11 04:18:24 -0700

    remove try/execpt keyword

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,16 +8,13 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    try:
 
-        db = MySQLdb.connect(host='localhost', user=sys.argv[1],
-                             password=sys.argv[2],
-                             database=sys.argv[3], port=3306)
-        db_c = db.cursor()
-        db_c.execute("SELECT * FROM states WHERE states.name='{}'\
-                ORDER BY states.id ASC".format(sys.argv[4]))
-        row_select = db_c.fetchall()
-        for row in row_select:
-            print(row)
-    except:
-        pass
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         password=sys.argv[2],
+                         database=sys.argv[3], port=3306)
+    db_c = db.cursor()
+    db_c.execute("SELECT * FROM states WHERE states.name='{}'\
+            ORDER BY states.id ASC".format(sys.argv[4]))
+    row_select = db_c.fetchall()
+    for row in row_select:
+        print(row)
