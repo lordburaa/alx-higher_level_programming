bd40f0b864a6c266c140ecfacdbdf438e247986a
lordburaa <comrdebiruk97@gmail.com>
2024-04-11 03:32:33 -0700

    fix pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,11 +7,13 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host = 'localhost', user=sys.argv[1], password=sys.argv[2], database=sys.argv[3], port=3306)
-    
+
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         password=sys.argv[2], database=sys.argv[3],
+                         port=3306)
     db_c = db.cursor()
     db_c.execute("SELECT * FROM states WHERE states.name LIKE 'n%'")
-    row_select=db_c.fetchall()
+    row_select = db_c.fetchall()
 
     for row in row_select:
         print(row)
