1db28fe136f38e830c73eee725919cd3991d0ad6
lordburaa <comrdebiruk97@gmail.com>
2024-04-12 01:16:25 -0700

    fix the pycodestyle

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
                          password=sys.argv[2],
                          database=sys.argv[3], port=3306)
     db_c = db.cursor()
-    db_c.execute("SELECT c.id, c.name, s.name FROM states s
+    db_c.execute("SELECT c.id, c.name, s.name FROM states s\
                  JOIN cities c on c.state_id=s.id")
     row_select = db_c.fetchall()
     for row in row_select:
