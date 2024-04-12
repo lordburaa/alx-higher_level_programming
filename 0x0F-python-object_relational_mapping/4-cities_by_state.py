66108155a47671e95fdb39c9d79cb1898bb0b75b
lordburaa <comrdebiruk97@gmail.com>
2024-04-12 01:14:17 -0700

    fix the eror

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+list all cities
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         password=sys.argv[2],
+                         database=sys.argv[3], port=3306)
+    db_c = db.cursor()
+    db_c.execute("SELECT c.id, c.name, s.name FROM states s
+                 JOIN cities c on c.state_id=s.id")
+    row_select = db_c.fetchall()
+    for row in row_select:
+        print(row)
