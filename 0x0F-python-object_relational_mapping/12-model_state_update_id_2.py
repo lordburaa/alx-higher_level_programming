794ce910cd54e3340d97b8db0a695333396ccfde
lordburaa <comrdebiruk97@gmail.com>
2024-04-22 07:24:56 -0700

    fix

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" changes the name of the STate object 
+""" changes the name of the STate object
 """
 
 from sqlalchemy import create_engine
@@ -8,9 +8,10 @@ from model_state import Base, State
 from sys import argv
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
     session = Session(bind=engine)
-    query =session.query(State).get(2)
+    query = session.query(State).get(2)
     query.name = 'New Mexico'
     session.add(query)
     session.commit()
