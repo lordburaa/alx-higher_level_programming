957fd1cc47ae9a0ccb5789b5d381c320c4b495ad
lordburaa <comrdebiruk97@gmail.com>
2024-04-22 06:06:38 -0700

    fix 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 """
-list all State object that contain the letter a from the database
+Get a State
 """
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from model_state import State, Base
+from model_state import Base, State
 from sys import argv
 
 
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]))
+    name = argv[4]
     session = Session(bind=engine)
-    query = session.query(State).filter(State.name.like('%a%'))
-    for row in query:
-        print("{}: {}".format(row.id, row.name))
+    query = session.query(State).filter(State.name == name).all()
+    if len(query) == 0:
+        print("Not found")
+    else:
+        print(query[1].id)
