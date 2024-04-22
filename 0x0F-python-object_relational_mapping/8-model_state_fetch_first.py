3d966de80bf37f2bf19d81d189c8dc33bc2d150e
lordburaa <comrdebiruk97@gmail.com>
2024-04-22 04:51:53 -0700

    fix hte pyocdestyle

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""prints the first State object form the database"""
+
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from model_state import State, Base
+from sys import argv
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    session = Session(bind=engine)
+    query = session.query(State).first()
+    print("{}: {}".format(query.id, query.name))
