a0b7e2dabbbe440f3235f75e02f08041cf1943a6
lordburaa <comrdebiruk97@gmail.com>
2024-04-22 00:52:04 -0700

    fix create table  state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,7 +5,7 @@
 # if there is error in the requirement
 # you can hange it column to mapped_collumn=> sqlalchemy.orm
 from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
@@ -13,5 +13,5 @@ class State(Base):
     """ state class """
 
     __tablename__ = "states"
-    id = Column(Integer, auto_generated, nullable=False)
-    name = Column(String(128), nullabel=False)
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
