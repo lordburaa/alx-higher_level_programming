b67e01799d470e47c015c8deff2aadf1a9f63f2e
lordburaa <comrdebiruk97@gmail.com>
2024-04-22 07:23:20 -0700

    changed the name

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" changes the name of the STate object 
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sys import argv
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3]))
+    session = Session(bind=engine)
+    query =session.query(State).get(2)
+    query.name = 'New Mexico'
+    session.add(query)
+    session.commit()
