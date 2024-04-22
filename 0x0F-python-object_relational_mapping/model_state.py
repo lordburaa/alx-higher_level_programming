fc510f1f8c084c4219c7bb48d48c072e8d8f6419
lordburaa <comrdebiruk97@gmail.com>
2024-04-22 00:33:28 -0700

    adding class

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# create new tbales;
+
+
+# if there is error in the requirement
+# you can hange it column to mapped_collumn=> sqlalchemy.orm
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import declarative_base
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = "states"
+    id = Column(Integer, auto_generated, nullable=False)
+    name = Column(String(128), nullabel=False)
