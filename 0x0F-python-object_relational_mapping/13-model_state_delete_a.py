4a625876cba07ed80fc4a036eed9d0a514dae530
lordburaa <comrdebiruk97@gmail.com>
2024-04-22 23:35:25 -0700

    fix the delete

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,7 +12,12 @@ from sys import argv
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3]))
     session = Session(bind=engine)
-    query = session.query(State).filter(State.name.like('%a%')).all()
+    query = session.query(State).filter(State.name.contains('a')).all()
+    for i in query:
+        session.delete(i)
+    session.commit()
+    """
     session.delete(query)
     session.commit()
     session.close()
+    """
