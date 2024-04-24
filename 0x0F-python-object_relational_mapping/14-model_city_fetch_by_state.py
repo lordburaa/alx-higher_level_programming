d70e007e713cf715e93a9d234df041c4347696f0
lordburaa <comrdebiruk97@gmail.com>
2024-04-23 23:35:23 -0700

    relationship

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+prints all city object from the databse
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_city import City
+from model_state import State, Base
+from sys import argv
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    session = Session(bind=engine)
+
+    query = session.query(State, City).join(City)
+    print(query)
+    for i in query:
+        print("{}: ({}) {}".format(i[0].name, i[0].id, i[1].name))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,16 +4,18 @@ create new tbales;
 if there is error in the requirement
 you can hange it column to mapped_collumn=> sqlalchemy.orm
 """
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-
+from model_state import State
 Base = declarative_base()
 
 
-class State(Base):
-    """ state class """
+class City(Base):
+    """ City class """
 
-    __tablename__ = "states"
+    __tablename__ = "cities"
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id))
+    state = relationship(State, backref="cities")
